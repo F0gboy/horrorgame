18b69d4641cd0b4dcbdd202a4990c2672022e3b7
leonardzz7 <leonard@leooli.dk>
2023-10-25 16:51:09 +0200

    WindowScript 50% done

---
+++ Assets/WindowCamScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowCamScript : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        if (transform.eulerAngles.x > 36 && transform.eulerAngles.x < 100)
+        {
+            
+            transform.eulerAngles = new Vector3(35, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+        if (transform.eulerAngles.x < 355 && transform.eulerAngles.x > 100)
+        {
+
+            transform.eulerAngles = new Vector3(-5, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        //if (transform.eulerAngles.y > 221)
+        //{
+
+        //    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 220, transform.eulerAngles.z);
+        //    Debug.Log("Error 3");
+        //}
+        //if (transform.eulerAngles.y < 141)
+        //{
+
+        //    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 140, transform.eulerAngles.z);
+        //    Debug.Log("Error 4");
+        //}
+
+    }
+}
